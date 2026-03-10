cf2a1465a1e4e2a6a998267efc6f252280e86273
Daniel Ahumada <danielahumada071@gmail.com>
2026-03-10 11:32:51 -0500

    iniciar springboot

---
+++ src/main/java/edu/dosw/proyecto/TechCupFutbolApplication.java
@@ -0,0 +1,12 @@
+package edu.dosw.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCupFutbolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCupFutbolApplication.class, args);
+    }
+}
