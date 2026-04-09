869b3e8dbd41a43e6f03ad46ca54c67aa8899e28
SrDark04 <rogermao5.1@gmail.com>
2026-04-09 12:39:16 -0500

    Finish the fixes

--- src/main/java/edu/eci/dosw/tech_cup/controller/UserController.java
+++ src/main/java/edu/eci/dosw/tech_cup/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/users")
@@ -42,8 +43,10 @@ public class UserController {
 
     @PostMapping
     @Operation(summary = "Crear usuario", description = "Registra un nuevo usuario")
-    public User createUser(@RequestBody UserRequest request) {
+    public User createUser(@RequestParam UserRequest request) {
+        System.out.println("ENTRE");
         return userService.createUser(toDto(request), request.password(), request.academicProgram());
+
     }
 
     @PutMapping("/{id}")
